b97fe0367b1b258e28edc51de8ddd5025e6a18c6
MarkSCQ <495431190@qq.com>
2023-03-04 12:22:22 +0800

    process2 todo make dataset

--- fileProcess.py
+++ fileProcess.py
@@ -8,8 +8,8 @@ wo3_renaming = {
     "甲醛": "formaldehyde",
     "汽油": "gasoline",
     "柴油": "diesel_fuel",
-    "H2":"h2",
-    "CH4":"ch4"
+    "H2": "h2",
+    "CH4": "ch4"
 }
 
 metal_ox_wo3 = "wo3"
@@ -22,29 +22,33 @@ metal_ox_wo3 = "wo3"
 
 # header=[]
 
-def translate_column(df,headnames):
 
-    
-    headnames = list(map(lambda x: x.replace(headnames[0], 'density_temp'), headnames))
-    headnames = list(map(lambda x: x.replace(headnames[1], 'gas_density'), headnames))
-    
+def translate_column(df, headnames):
+
+    headnames = list(map(lambda x: x.replace(
+        headnames[0], 'density_temp'), headnames))
+    headnames = list(map(lambda x: x.replace(
+        headnames[1], 'gas_density'), headnames))
+
     for idx in range(len(headnames)):
-        # nsp = 
+        # nsp =
         element_pairs = headnames[idx].split("#")
-        if len(element_pairs)>1:
-            headnames[idx]=element_pairs[1] 
-    
+        if len(element_pairs) > 1:
+            headnames[idx] = element_pairs[1]
+
     return headnames
 
+
 def columns_reshape(df):
-    df =  df.copy()
+    df = df.copy()
 
-    df[['density','temperature']] = df.density_temp.str.split(",",expand=True)
+    df[['density', 'temperature']] = df.density_temp.str.split(
+        ",", expand=True)
     df = df.drop(['density_temp'], axis=1)
-    df['gas_density'] = df['gas_density'].apply(lambda x:x[:-3])
+    df['gas_density'] = df['gas_density'].apply(lambda x: x[:-3])
 
-    df['density']=df['density'].apply(lambda x:x[:-3])
-    df['temperature']=df['temperature'].apply(lambda x:x[:-1])
+    df['density'] = df['density'].apply(lambda x: x[:-3])
+    df['temperature'] = df['temperature'].apply(lambda x: x[:-1])
 
     header = df.columns.tolist()
     header.insert(0, header[-1])
@@ -55,25 +59,26 @@ def columns_reshape(df):
     return df
     # return target
 
+
 def fill_blanks_df(df, col):
     curr_val = 0
     for r in range(len(df)):
         # if not nan, update mod_val
-        curr_cell =df.iloc[r,col]
+        curr_cell = df.iloc[r, col]
         if not pd.isna(curr_cell):
             # currflag = "250℃"
             curr_val = curr_cell
             # df.iloc[r,col]=curr_val
         else:
             # if nan, set val
-            df.iloc[r,col]=curr_val
+            df.iloc[r, col] = curr_val
     return df
 
 # def fill_blanks(df_list):
 #     d_list = []
 #     for d in df_list:
 #         d_list.append(fill_blanks_df(df,0))
-    
+
 #     return d_list
 
 
@@ -88,8 +93,8 @@ def add_metal_ox(df, metal=""):
 def df_add_elements(df, gas, metal):
     add_gas(df, gas)
     add_metal_ox(df, metal)
-    
-    
+
+
 def renaming_title(df):
     curr_df_header = list(df.head())
 
@@ -108,64 +113,63 @@ def read_process(path, renaming_dict, metal):
         if k in wo3_renaming:
             new_name = wo3_renaming[k]
             curr_df = pd.read_excel(path, sheet_name=k)
-            
+
             curr_head = list(curr_df.head())
-            
-            translated = translate_column(curr_df,curr_head)
+
+            translated = translate_column(curr_df, curr_head)
             curr_df.columns = translated
-            
+
             fill_blanks_df(curr_df, 0)
-            curr_df=columns_reshape(curr_df)
+            curr_df = columns_reshape(curr_df)
             df_add_elements(curr_df, new_name, metal)
 
             dataframes[new_name] = curr_df
             global header
             header = list(curr_df.head())
             # print(header)
-            
-    return dataframes
 
+    return dataframes
 
 
 def make_dataset_header():
     curr_header = header[:5]
-    curr_header.insert(2,"element")
+    curr_header.insert(2, "element")
     curr_header.append("resp_val")
     return curr_header
 
 
 def sheet_dataset(df):
-    dataset=[]
-    elements= list(df.head())
+    dataset = []
+    elements = list(df.head())
     print(elements)
     for row in range(len(df)):
-        # print(raw_gas_exp.iloc[[row]])
         data = list(df.iloc[row])
-        # print(data)
-        # print(tmp)
         tmp = data[0:5]
         for col in range(5, len(data)):
             tmp.append(elements[col])
 
             tmp.append(data[col])
             dataset.append(tmp)
-            tmp = data[0:5]    
+            tmp = data[0:5]
     print(dataset[:3])
     return dataset
+
+
 def make_dataset(dfs):
-    print("dfs size  ",len(dfs))
+    print("dfs size  ", len(dfs))
 
     ds_header = make_dataset_header()
     dataset = []
     for d in dfs:
         sub_set = sheet_dataset(dfs[d])
         print(len(sub_set))
-        dataset+=sub_set
+        dataset += sub_set
     print(len(dataset))
     print(dataset[:4])
-    df = pd.DataFrame(dataset, columns =['metal_ox', 'gas_cate','temperature','density','resp_density','metal','resp_val']) 
+    df = pd.DataFrame(dataset, columns=[
+                      'metal_ox', 'gas_cate', 'temperature', 'density', 'resp_density', 'metal', 'resp_val'])
     print(df[:10])
-    df.to_csv("dataset.csv",index=False)
+    df.to_csv("dataset.csv", index=False)
     # for i in dataset:
     #     print(i)
     # print(ds_header)
@@ -175,7 +179,8 @@ def make_dataset(dfs):
 # read dataframes
 # def readXlsx(path, renaming_dict, gas, metal):
 
-dataframes = read_process("./files_未调整格式/WO3.xlsx", wo3_renaming,metal_ox_wo3)
+
+dataframes = read_process("./files_未调整格式/WO3.xlsx", wo3_renaming, metal_ox_wo3)
 
 make_dataset(dataframes)
 # dataframes
--- fileProcess2.py
+++ fileProcess2.py
@@ -2,3 +2,66 @@
 import pandas as pd
 
 
+TIO2_renaming = {
+    "苯": "benzene",
+    "甲苯": "toluene",
+    "二甲苯": "xylene",
+    "甲醛": "formaldehyde",
+    "汽油": "gasoline",
+    "柴油": "diesel_fuel",
+    "H2":"h2",
+    "CH4":"ch4"
+}
+
+
+
+metal_ox = "tio2"
+
+
+def translateHeader(df):
+    header = ['metal_ox', 'gas_cate', 'temperature', 'density', 'resp_density']
+    # , 'metal', 'resp_val'
+    df_header = list(df.head())
+    for i in range(len(header)):
+        df_header[i]=header[i]
+    
+    for i in range(len(df_header)):
+        strsplit = df_header[i].split("+")
+        if len(strsplit)>1:
+            df_header[i]=strsplit[1]
+    # print(df_header)
+    return df_header
+
+
+def translateCasCate(df,sheetname):
+    if sheetname in TIO2_renaming:
+        # print(TIO2_renaming[sheetname])
+        df['gas_cate']=TIO2_renaming[sheetname]
+    # print(df[:3])
+    
+
+def read_process(path):
+
+    SheetNameDF = {}
+
+    xl = pd.read_excel(path, None)
+
+    sheetnames = xl.keys()
+
+    dataframes = {}
+
+    for k in sheetnames:
+        curr_df = pd.read_excel(path, sheet_name=k)
+        eng_header = translateHeader(curr_df)
+        curr_df.columns = eng_header
+        translateCasCate(curr_df, sheetname=k)
+        print(curr_df[:4])
+
+    return dataframes
+
+
+path = "./files_需要转换成数据/TIO2.xlsx"
+
+
+read_process(path=path)
+
